d0cd0b5ad9e3fa81e7d3093e2ef4e9aae87a3fc3
maxfly60 <maxence.gelig@gmail.com>
2021-02-11 00:34:36 +0100

    Ajout des commentaires EXO 1 - PHP

--- fonctions.php
+++ fonctions.php
@@ -5,28 +5,41 @@
 /*
     Afin de tester la route :
 
-        localhost/username/test/Adrield     ->      fonctionne, artiste présent dans la BDD, retourne {"exists":true,"idUser":"4915"}
+        localhost/username/test/Adrield     ->      fonctionne, artiste présent dans la BDD, retourne 
+                                                        {"L'user Adrield est present dans la BDD ":true," son idUser est ":"4915"}
 
-        localhost/username/test/maxfly      ->      ne fonctionne pas, artiste pas présent dans la BDD, retourne {"exists":false}
-
+        localhost/username/test/maxfly      ->      ne fonctionne pas, artiste pas présent dans la BDD, retourne 
+                                                        {"L'user maxfly est present dans la BDD ":false}
 */
-Flight::route('GET /username/test/@name', function($name){
+
+// Route permettant de verifier si un nom est bien présent dans la base de données.
+Flight::route('GET /username/test/@name', function($name){     
+    
+    // On fait une requete pour verifier si le nom demandé dans l'URL existe dans la base.
     $db = Flight::get('db');
-    $exists = FALSE;
+    $existe = FALSE;
     $requete = $db->prepare('SELECT idUser FROM User WHERE NomUser = ?');
     $requete->execute(array($name));
+
+    // $donnees récupère la ligne correspondant au nom demandé dans l'URL si celui ci est dans la BDD.
+    // Si le nom n'est pas présent dans la BDD, alors $donnees récupère NULL                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PAS SUR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     while ($donnees = $requete->fetch())
     {
+        // La variable idUser prend la valeur correspondant à l'id dans le tableau $donnees
         $idUser = $donnees['idUser'];
+        
+        // Si la variable $idUser n'est pas NULL, alors le booléen $existe est positif. 
         if(isset($idUser)) {
-            $exists = true;
+            $existe = true;
         }
     }
-    if($exists) {
-        echo $name;
-        Flight::json(array('exists' => true, 'idUser' => $idUser));
+
+    // Si $existe est positif, alors on affiche un objet json personnalisé en donnant la variable $name (le nom dans l'URL), ainsi que l'id associé dans la base de données.
+    // Si $existe est négatif, alors on affiche un objet json personnalisé indiquant le nom n'est pas présent dans la base de données.
+    if($existe) {
+        Flight::json(array("L'user $name est present dans la BDD " => true, ' son idUser est ' => $idUser));
     } else {
-        Flight::json(array('exists' => false));
+        Flight::json(array("L'user $name est present dans la BDD " => false));
     }
 });
 
